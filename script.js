b28eeeed88540589623bf2cca7eb5d5b466b3370
Lil <loonaarc@gmail.com>
2025-10-26 19:01:28 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function generate() {
+    const length = document.getElementById("length").value;
+
+    const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
+
+    let password = "";
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * chars.length);
+        password += chars[randomIndex];
+    }
+
+    document.getElementById("password").innerText = password;
+    document.getElementById("copyBtn").style.display = "block";
+}
+
+function copyToClipBoard() {
+    const password = document.getElementById("password").innerText;
+    navigator.clipboard.writeText(password).then(() => {
+        document.getElementById("message").innerText = "Password copied to clipboard!";
+    });
+}
